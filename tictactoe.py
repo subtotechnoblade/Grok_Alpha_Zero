75a7539b41ea2e788d708173d57ca71349983100
Brianwu24 <brian.wu24@ycdsbk12.ca>
2024-10-22 00:42:58 -0400

    Functions

--- tictactoe.py
+++ tictactoe.py
@@ -19,9 +19,9 @@ board = np.zeros((3, 3), dtype=np.int8)
 # [[0 0 0]
 #  [0 0 0]
 #  [0 0 0]]
-print("Starting Board")
-print(board)
-print("--"*10)
+# print("Starting Board")
+# print(board)
+# print("--"*10)
 # Next thing
 
 board[0][0] = -1
@@ -33,8 +33,35 @@ board[1][0] = 1
 board[1][2] = -1
 board[2][2] = 1
 board[2][1] = -1
-print(board)
-print("draw")
+# print(board)
+# print("draw")
+
+#todo functions: make a place move function that takes:
+# the board
+# coordinate x, y
+# and the current player as -1 or 1
+# places the move onto the board
+# returns the board with the newly placed board
+
+
+# Resources
+# making a function
+def my_func():
+ return 100 # this can be anything, including numpy arrays
+
+# calling a function
+my_func()
+x = my_func() # this will be 100
+print(x)
+
+# more examples
+def add(a, b):
+ return a + b
+
+result = add(1, 1) # result should be 2
+print(result)
+
+
 
 
 
