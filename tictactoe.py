682f093e58a94a753840c745f405838486bd0e98
Brianwu24 <brian.wu24@ycdsbk12.ca>
2024-10-21 23:45:26 -0400

    Play a game against yourself

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,47 @@
 import numpy as np
-# todo Make Board
-print(np.arange(10))
+
+# make zeros array np.zeros(shape)
+# make ones array np.ones(shape)
+# make array from list np.array(list)
+
+# human friendly board, not computer friendly
+[["E", "E", "E"]]
+[["E", "X", "O"]]
+[["E", "X", "O"]]
+
+[[0, 0, 0],
+ [0, 0, 0],
+ [0, 0, 0]]
+# pieces with -1, 0, 1
+
+board = np.zeros((3, 3), dtype=np.int8)
+# numpy array board
+# [[0 0 0]
+#  [0 0 0]
+#  [0 0 0]]
+print("Starting Board")
+print(board)
+print("--"*10)
+# Next thing
+#todo Place a move onto the board, play a game against yourself, and check who win
+# This is how to place a move board[0][0] = -1
+
+
+print(board)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# todo Jump Ian Patrick Tang
+
 
 
