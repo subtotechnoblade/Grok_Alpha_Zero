56bd8b79003090554a3cc41c5fc10f480a779eae
Brianwu24 <brian.wu24@ycdsbk12.ca>
2024-10-23 22:45:37 -0400

    Classes, methods vs functions.

--- tictactoe.py
+++ tictactoe.py
@@ -57,26 +57,53 @@ def func(board, x, y, player):
     board[y][x] = player
     return board
 
+# function vs method
+# function
+
+def foo(inputs):
+    return inputs ** 2
+
+# class
+class House:
+    def __init__(self, new_owner): # constructor
+        self.owner = "Brian" # Brian
+        self.owner = new_owner # Jessy 2.0, over writes Brian
+
+house = House("Jessy 2.0") # this is calling constructor
+# print(house.owner)
 
 
+class TicTacToe:
+    def __init__(self):
+        self.current_player = -1
+        self.board = np.zeros((3, 3), dtype=np.int8)
+        # doesn't return anything
 
+    def place_move(self, x, y):
+        self.board[y][x] = self.current_player
+        self.current_player = self.current_player * -1
 
-
-
-
-
-
-
-
-
-
-
-
-
+game = TicTacToe()
+game.place_move(1, 1)
+# game.place_move(0, 0)
+# game.place_move(0, 0)
+# print(game.board)
 
 
 
 # todo Jump Ian Patrick Tang
 
 
+class Container:
+    def __init__(self, filter):
+        self.filter = filter
+        self.storage = []
 
+    def add(self, item):
+        if item in self.filter:
+            self.storage.append(item)
+
+
+container = Container([2, 3, 5, 7, 11])
+container.add(2)
+print(container.storage)
