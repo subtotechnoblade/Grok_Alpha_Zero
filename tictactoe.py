e0b2c455348d989ce5fe28c656adc0a707cef689
jesss <jessicaz8319@gmail.com>
2024-10-23 22:10:14 -0400

    Game of tictactoe

--- tictactoe.py
+++ tictactoe.py
@@ -1,20 +1,20 @@
 import numpy as np
-
+#
 # make zeros array np.zeros(shape)
 # make ones array np.ones(shape)
 # make array from list np.array(list)
 
 # human friendly board, not computer friendly
-[["E", "E", "E"]]
-[["E", "X", "O"]]
-[["E", "X", "O"]]
-
-[[0, 0, 0],
- [0, 0, 0],
- [0, 0, 0]]
+# [["E", "E", "E"]]
+# [["E", "X", "O"]]
+# [["E", "X", "O"]]
+#
+# [[0, 0, 0],
+#  [0, 0, 0],
+#  [0, 0, 0]]
 # pieces with -1, 0, 1
 
-board = np.zeros((3, 3), dtype=np.int8)
+# board = np.zeros((3, 3), dtype=np.int8)
 # numpy array board
 # [[0 0 0]
 #  [0 0 0]
@@ -23,16 +23,16 @@ board = np.zeros((3, 3), dtype=np.int8)
 # print(board)
 # print("--"*10)
 # Next thing
-
-board[0][0] = -1
-board[1][1] = 1
-board[0][1] = -1
-board[0][2] = 1
-board[2][0] = -1
-board[1][0] = 1
-board[1][2] = -1
-board[2][2] = 1
-board[2][1] = -1
+# #
+# board[0][0] = -1
+# board[1][1] = 1
+# board[0][1] = -1
+# board[0][2] = 1
+# board[2][0] = -1
+# board[1][0] = 1
+# board[1][2] = -1
+# board[2][2] = 1
+# board[2][1] = -1
 # print(board)
 # print("draw")
 
@@ -42,24 +42,26 @@ board[2][1] = -1
 # and the current player as -1 or 1
 # places the move onto the board
 # returns the board with the newly placed board
+#
+# def b_func(board, x, y, player):
+#     board = [[0, 0, 0],
+#              [0, 0, 0],
+#              [0, 0, 0]]
+#     board[x][y] = player
+#     print(board)
+#
+# b_func(2, 1, 1)
+
+outerboard = np.zeros((3, 3), dtype=np.int8)
+def func(board, x, y, player):
+    board[y][x] = player
+    return board
 
 
-# Resources
-# making a function
-def my_func():
- return 100 # this can be anything, including numpy arrays
 
-# calling a function
-my_func()
-x = my_func() # this will be 100
-print(x)
 
-# more examples
-def add(a, b):
- return a + b
 
-result = add(1, 1) # result should be 2
-print(result)
+
 
 
 
