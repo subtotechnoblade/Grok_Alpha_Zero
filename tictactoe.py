edf8394bd5246f8825aa1e3ebe232b3f89fb19ea
TryHarder <brian8204001@gmail.com>
2024-11-05 23:18:48 -0500

    Updated tictactoe.py with the full methods from Guide.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from numba import njit
 # make zeros array np.zeros(shape)
 # make ones array np.ones(shape)
 # make array from list np.array(list)
@@ -85,12 +85,87 @@ class TicTacToe:
 
         # todo make move_history and add the played move everytime do_action is called
 
+    def get_current_player(self):
+        # returns the current player
+        pass
+
+    def get_legal_actions(self):
+        # returns the all possible legal actions in a list [action1, action2, ..., actionN] given self.board
+        # Note that this action will be passed into do_action() and do_action_MCTS
+        # MAKE SURE THERE are no duplicates (pretty self explanatory)
+        pass
+
+    @staticmethod
+    # @njit(cache=True)
+    def get_legal_actions_policy_MCTS(board: np.array, policy: np.array, shuffle=False):
+        """
+        THIS PART IS REALLY HARD FOR BEGINNERS, I RECOMMEND TO SKIP THIS PART UNTIL YOU ARE MORE CONFIDENT
+        :param board: numpy array of the board
+        :param policy: a numpy array of shape = self.policy shape defined in __init__, straight from the neural network's policy head
+        :param shuffle: You might want to shuffle the policy and legal_actions because the last index is where the search starts
+        if it is too hard you don't implement anything much will happen, its just that randomizing might improve convergence just by a but
+        :return: legal_actions as a list [action0, action1, ...], child_prob_prior as a numpy array
+
+        In essence, index 0 in legal_actions and child_prob_prior should be the probability of the best move for that legal action
+        so for example in tic tac toe
+        legal_actions =             [0,     1,   2,   3,    4,    5,    6,    7,    8,    9]
+        child_prob_prior / policy = [0.1, 0.1, 0.3, 0.2, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05] (I made these up, shape=(9,))
+        it means that action 0 will have a predicted chance of 10% of being the best move, 1 will have 10%, 2 will have 30% and so on
+        Note that this is a probability distribution, after removing the un wanted actions you have to normalize it to sum up to 1
+        do that with policy /= np.sum(policy)
+
+
+        Anyway, if your self.policy shape matches the action shape you can just return the policy without doing anything
+        recommend to use numba njit to speed up this method for MCTS
+        """
+
+        # example with randomization for tictac toe:
+        # assuming policy from the comments above
+        # board = board.reshape((-1)) flatten the 3 by 3 board into a length 9 array
+        # policy = policy.reshape(-1) flatten the policy
+        # policy = policy[board == 0] keep the values that correspond to an empty part of the board
+        # policy /= np.sum(policy) # normalize it
+        # legal_actions = np.argwhere(board == 0) # get the indexes where the board is empty
+        # if shuffle:
+            # shuffled_indexes = np.random.permutation(len(legal_actions)) # create random indexes
+            # legal_actions, policy = legal_actions[shuffled_indexes], policy[shuffled_indexes] # index the arrays to shuffled them
+        # return legal_moves, policy
+
+        # MAKE SURE THERE ARE NO DUPLICATES because it is going to increase the tree complexity thus slowing things down
+        # it is also going to give a wrong policy and weird errors might occur
+        # for checking use
+        # assert len(legal_actions) == len(set(legal_action))
+        # set cannot have any duplicates and thus removed so if the lengths are different the there is a problem
+        pass
+
     def do_action(self, action): # must conform to this format - Brian
         x, y = action
         self.board[y][x] = self.current_player
         self.current_player = self.current_player * -1
 
         # Implement action_history
+
+    @staticmethod
+    # @njit(cache=True)
+    def do_action_MCTS(board, action, current_player):
+        # this is for the monte carlo tree search's
+        pass
+
+    def get_input_state(self):
+        # gets the numpy array for the neural network
+        # for now just return the board as a numpy array
+        # Brian will probably implement this later for specific neural networks
+        # RWKV can just take in the board without problem
+        # the original alphazero's network required the past boards
+        # Uses in the root node of MCTS
+        pass
+
+    @staticmethod
+    # @njit(cache=True)
+    def get_input_state_MCTS(board):
+        # Used for retrieving the state for any child nodes (not the root)
+        # just return the board from the inputs
+        return board
 
     def check_win(self):
         # for getting a row use indexing board [0]
@@ -103,6 +178,22 @@ class TicTacToe:
         if all(self.board != 0):
             return 0
         raise NotImplementedError("Implement check win with other methods")
+
+    @staticmethod
+    # @njit(cache=True)
+    def check_win_MCTS(board, last_action):
+        #Used to check if MCTS has reached a terminal node
+        pass
+    @staticmethod
+    @njit(cache=True)
+    def get_winning_actions_MCTS(board, current_player, fast_check=False):
+        # Brian will be looking very closely at this code when u implement this
+        # reocmment to use check_win_MCTS unless there is a more efficient way
+        # making sure that this doesn't slow this MCTS to a halt
+        # if your game in every case only has 1 winning move you don'y have to use fast_check param
+        # please do not remove the fast_check parameter
+        # check the gomoku example for more info
+        pass
 
 
 game = TicTacToe()
