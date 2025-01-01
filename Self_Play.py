5630b7a1348ae63beba14f00d49b375ee400cdae
TryHarder <brian8204001@gmail.com>
2025-01-01 18:02:25 -0500

    Partially done, still working on the multiprocessing part

--- Self_Play.py
+++ Self_Play.py
@@ -1,1 +1,176 @@
+import os
+import numpy as np
+import h5py as h5
 import onnxruntime as rt
+import multiprocessing as mp
+
+from MCTS import MCTS
+
+from Gomoku import Gomoku
+class Self_Play:
+    def __init__(self,
+                 game,
+                 build_config:dict,
+                 train_config:dict,
+                 lock: mp.Lock,
+                 folder_path:str,
+                 generation:int):
+        self.game: Gomoku = game
+        self.build_config = build_config
+        self.train_config = train_config
+        self.lock = lock
+        self.folder_path = folder_path
+        self.generation = generation
+
+        if not os.path.exists(f"{folder_path}/{generation}/Self_Play_Data.h5"):
+            raise ValueError("Dataset file hasn't been created")
+
+        if train_config["use_gpu"]:
+            if train_config["use_tensorrt"]:
+                providers = [
+                    ('TensorrtExecutionProvider', {
+                        "trt_engine_cache_enable": True,
+                        "trt_dump_ep_context_model": True,
+                        "trt_builder_optimization_level": 5,
+                        "trt_auxiliary_streams": 0,
+                        "trt_ep_context_file_path": f"{folder_path}/{generation}/TRT_cache/"
+                    }),
+                    'CUDAExecutionProvider',
+                    'CPUExecutionProvider']
+            else:
+                providers = [
+                    'CUDAExecutionProvider',
+                    'CPUExecutionProvider']
+        else:
+            providers = ['CPUExecutionProvider']
+
+        self.sess = rt.InferenceSession(f"{self.folder_path}/{self.generation}/TRT_cache/model_ctx.onnx",
+                                        providers=providers)
+
+        self.iteration_limit = self.train_config["MCTS_iteration_limit"]
+        self.time_limit = self.train_config["MCTS_time_limit"]
+        self.mcts: MCTS = MCTS(self.game,
+                               self.build_config,
+                               self.sess,
+                               c_puct_init=self.train_config["c_puct_init"],
+                               use_dirichlet=True,
+                               dirichlet_alpha=self.train_config["dirichlet_alpha"],
+                               tau=1.0,
+                               fast_find_win=False,
+                               use_njit=self.train_config["use_njit"])
+
+    def play(self):
+        board_states = []
+        improved_policies = []
+        target_values = []
+
+        winner = -2
+        while winner != -2:
+            board_states.append(self.game.get_input_state().copy())
+
+            current_move_num = len(self.game.action_history)
+            if current_move_num < self.train_config["num_explore_moves"]:
+                tau = 1.0 - (0.5 * (current_move_num / self.train_config["num_explore_moves"]))
+                self.mcts.update_hyperparams(self.mcts.c_puct_init, tau)
+            else:
+                self.mcts.update_hyperparams(self.mcts.c_puct_init, 5e-3)
+
+            action, move_probs = self.mcts.run(iteration_limit=self.iteration_limit,
+                                               time_limit=self.time_limit,
+                                               use_bar=False)
+
+            move_probs = map(lambda x: x[:2], move_probs) # This takes the first and seconds element of which is the [action, prob]
+            improved_policy = self.game.compute_policy_improvement(move_probs)
+            improved_policies.append(improved_policy)
+
+            target_values.append(self.game.get_current_player()) # Important that this is before do_action()
+            # We can safely say that target_values are the players that played the move, not the next player
+
+            self.game.do_action(action)
+            print(self.game.board)
+
+            self.mcts.prune_tree(action)
+            winner = self.game.check_win()
+
+            if winner != -2: # there is a winner
+                board_states = np.array(board_states, dtype=board_states[0].dtype)
+                improved_policies = np.array(improved_policies)
+                target_values = np.array(target_values, dtype=np.int8)
+
+                if winner ==  target_values[-1] == -1: # if player -1 just won
+                    target_values *= -1 # Flip it so that the player that won, evaluates to 1 (winner)
+                # else the player that played was 1, and won which is 1, thus no need to invert
+
+        # Assume that a .h5 file has been created and the max moves dataset is already created
+
+        with self.lock, h5.File(f"{self.folder_path}/{self.generation}/Self_Play_Data.h5", "r+") as file:
+            game_length = len(self.game.action_history)
+            if file["max_moves"][0] < game_length:
+                file["max_moves"][0] = game_length
+
+            dataset_name = f"{(len(file.keys()) - 1) // 3}"
+
+            file.create_dataset(f"board_states_{dataset_name}",
+                                shape=(game_length, *self.game.get_input_state().shape),
+                                dtype=board_states.dtype,
+                                data=board_states)
+
+            file.create_dataset(f"policies_{dataset_name}",
+                                shape=(game_length, self.game.policy_shape[0]),
+                                dtype=np.float32,
+                                data=improved_policies)
+
+            file.create_dataset(f"values_{dataset_name}",
+                                shape=(game_length, 1),
+                                dtype=np.float32,
+                                data=target_values)
+
+
+def self_play_task(game_id, game_class,
+                 build_config:dict,
+                 train_config:dict,
+                 lock: mp.Lock,
+                 folder_path:str,
+                 generation:int):
+
+    print(game_id)
+    task = Self_Play(game_class(),
+                     build_config,
+                     train_config,
+                     lock,
+                     folder_path,
+                     generation)
+    task.play()
+
+def run_self_play(
+                  game_class,
+                  build_config,
+                  train_config,
+                  folder_path,
+                  generation,
+                  num_processes=1):
+    lock = mp.Lock()
+    results = []
+
+
+    # results = [pool.apply_async(self_play_task, args=(game_id, game_class, build_config, train_config, lock, folder_path, generation)) for game_id in range(train_config["games_per_generation"])]
+    # for r in results:
+    #     r.wait()
+
+if __name__== "__main__":
+    # Testing code for validation
+    from Gomoku.Gomoku import Gomoku, build_config, train_config
+
+    folder_path = "Gomoku/Grok_Zero_Train/"
+    run_self_play(Gomoku, build_config, train_config, folder_path, 0)
+
+
+
+
+
+
+
+
+
+
+
