1d53c2ab02d37c6bea1dc063f1afb3e2ff702dc9
TryHarder <brian8204001@gmail.com>
2024-12-31 20:46:46 -0500

    Added padding param

--- Net/Batched_Net_Infer.py
+++ Net/Batched_Net_Infer.py
@@ -21,17 +21,17 @@ class Batch_Conv1D(tf.keras.layers.Layer):
         return self.conv1D(inputs)
 
 class Batch_Conv2D(tf.keras.layers.Layer):
-    def __init__(self, filters, kernel_size=(1, 1), strides=(1, 1), use_bias=True, **kwargs):
+    def __init__(self, filters, kernel_size=(1, 1), strides=(1, 1), padding="same", use_bias=True, **kwargs):
         super().__init__(**kwargs)
-        self.conv2D= tf.keras.layers.Conv2D(filters, kernel_size, strides, use_bias=use_bias)
+        self.conv2D= tf.keras.layers.Conv2D(filters, kernel_size, strides, padding=padding, use_bias=use_bias)
 
     def call(self, inputs):
         return self.conv2D(inputs)
 
-class Batch_Conv2D(tf.keras.layers.Layer):
-    def __init__(self, filters, kernel_size=(1, 1, 1), strides=(1, 1, 1), use_bias=True, **kwargs):
+class Batch_Conv3D(tf.keras.layers.Layer):
+    def __init__(self, filters, kernel_size=(1, 1, 1), strides=(1, 1, 1), padding="same", use_bias=True, **kwargs):
         super().__init__(**kwargs)
-        self.conv3D= tf.keras.layers.Conv3D(filters, kernel_size, strides, use_bias=use_bias)
+        self.conv3D= tf.keras.layers.Conv3D(filters, kernel_size, strides, padding=padding, use_bias=use_bias)
 
     def call(self, inputs):
         return self.conv3D(inputs)
