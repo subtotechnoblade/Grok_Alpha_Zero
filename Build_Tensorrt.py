787740e2ad7cb6f0d7b0ee994c82a436fcfa63ab
TryHarder <brian8204001@gmail.com>
2025-01-02 17:08:04 -0500

    Added dynamic shapes range to tensorrt utilizing the num_workers in train config

--- Build_Tensorrt.py
+++ Build_Tensorrt.py
@@ -5,17 +5,28 @@ import onnxruntime as rt
 
 def cache_tensorrt(game,
                    build_config,
+                   train_config,
                    folder_path,
                    generation,
                    warmup_iterations=10):
     # uses the generation number to build the generation cache
+    embed_size = build_config["embed_size"]
+    num_heads = build_config["num_heads"]
+    num_layers = build_config["num_layers"]
+
+    max_shape = opt_shape = train_config["num_workers"]
     providers = [
         ('TensorrtExecutionProvider', {
         "trt_engine_cache_enable":True,
         "trt_dump_ep_context_model": True,
         "trt_builder_optimization_level": 5,
         "trt_auxiliary_streams": 0,
-        "trt_ep_context_file_path": f"{folder_path}/{generation}/TRT_cache/"
+        "trt_ep_context_file_path": f"{folder_path}/{generation}/TRT_cache/",
+
+            "trt_profile_min_shapes": f"inputs:1x15x15,input_state:{num_layers}x2x1x{embed_size},input_state_matrix:{num_layers}x1x{num_heads}x{embed_size // num_heads}x{embed_size // num_heads}",
+            "trt_profile_max_shapes": f"inputs:{max_shape}x15x15,input_state:{num_layers}x2x{max_shape}x{embed_size},input_state_matrix:{num_layers}x{max_shape}x{num_heads}x{embed_size // num_heads}x{embed_size // num_heads}",
+            "trt_profile_opt_shapes": f"inputs:{opt_shape}x15x15,input_state:{num_layers}x2x{opt_shape}x{embed_size},input_state_matrix:{num_layers}x{opt_shape}x{num_heads}x{embed_size // num_heads}x{embed_size // num_heads}",
+
         }),
         'CUDAExecutionProvider',
         'CPUExecutionProvider']
@@ -31,10 +42,6 @@ def cache_tensorrt(game,
 
     dummy_inputs = np.random.uniform(-1, 2, (1, *game.get_input_state().shape)).astype(np.float32)
 
-    embed_size = build_config["embed_size"]
-    num_heads = build_config["num_heads"]
-    num_layers = build_config["num_layers"]
-
     input_state = np.zeros((num_layers, 2, 1, embed_size), dtype=np.float32)
     input_state_matrix = np.zeros((num_layers, 1, num_heads, embed_size // num_heads, embed_size // num_heads), dtype=np.float32)
     for _ in range(warmup_iterations):
@@ -44,14 +51,30 @@ def cache_tensorrt(game,
                                                                 "input_state_matrix": input_state_matrix})
     print("Successfully built trt engine!")
 
-def get_speed(game, build_config, folder_path, generation, iterations=100):
+def get_speed(game,
+              build_config,
+              train_config,
+              folder_path,
+              generation,
+              iterations=100):
+
+    embed_size = build_config["embed_size"]
+    num_heads = build_config["num_heads"]
+    num_layers = build_config["num_layers"]
+
+    max_shape = opt_shape = train_config["num_workers"]
     providers = [
         ('TensorrtExecutionProvider', {
         "trt_engine_cache_enable":True,
         "trt_dump_ep_context_model": True,
         "trt_builder_optimization_level": 5,
         "trt_auxiliary_streams": 0,
-        "trt_ep_context_file_path": f"{folder_path}/{generation}/TRT_cache/"
+        "trt_ep_context_file_path": f"{folder_path}/{generation}/TRT_cache/",
+
+        "trt_profile_min_shapes": f"inputs:1x15x15,input_state:{num_layers}x2x1x{embed_size},input_state_matrix:{num_layers}x1x{num_heads}x{embed_size // num_heads}x{embed_size // num_heads}",
+        "trt_profile_max_shapes": f"inputs:{max_shape}x15x15,input_state:{num_layers}x2x{max_shape}x{embed_size},input_state_matrix:{num_layers}x{max_shape}x{num_heads}x{embed_size // num_heads}x{embed_size // num_heads}",
+        "trt_profile_opt_shapes": f"inputs:{opt_shape}x15x15,input_state:{num_layers}x2x{opt_shape}x{embed_size},input_state_matrix:{num_layers}x{opt_shape}x{num_heads}x{embed_size // num_heads}x{embed_size // num_heads}",
+
         }),
         'CUDAExecutionProvider',
         'CPUExecutionProvider']
@@ -61,10 +84,6 @@ def get_speed(game, build_config, folder_path, generation, iterations=100):
                                providers=providers)
 
     dummy_inputs = np.random.uniform(-1, 2, (1, *game.get_input_state().shape)).astype(np.float32)
-
-    embed_size = build_config["embed_size"]
-    num_heads = build_config["num_heads"]
-    num_layers = build_config["num_layers"]
 
     input_state = np.zeros((num_layers, 2, 1, embed_size), dtype=np.float32)
     input_state_matrix = np.zeros((num_layers, 1, num_heads, embed_size // num_heads, embed_size // num_heads), dtype=np.float32)
