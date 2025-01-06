bde7c5a151b375e6358453f1cb4b194b625baf10
TryHarder <brian8204001@gmail.com>
2025-01-06 16:26:21 -0500

    Changed from retesting, we attempt to wait using per_process_wait_time to wait for the process, if the wait time is up continue with what we have to inference

--- Client_Server.py
+++ Client_Server.py
@@ -1,4 +1,5 @@
 import os
+import time
 import numpy as np
 import onnxruntime as rt
 
@@ -57,7 +58,8 @@ class Server:
                  shared_memories: list[SharedMemory, ...],
                  providers,
                  sess_options,
-                 file_path):
+                 file_path,
+                 per_process_wait_time=0.001):
 
         # batch dim is denoted with -1
 
@@ -104,11 +106,13 @@ class Server:
         self.providers = providers
         self.num_workers = len(self.shms)
         self.file_path = file_path
-
+        self.wait_time = per_process_wait_time * self.num_workers
 
         self.sess = rt.InferenceSession(f"{self.file_path}",
                                         sess_options=sess_options,
                                         providers=providers)
+
+
 
     def compute_transposition_to_standard(self, new_shape: list):
         # original being batch dim at index 0
@@ -146,26 +150,21 @@ class Server:
             inactivate_connections = []
             batched_input_feed = {name:[] for name in self.inputs_feed_info.keys()}
 
-            while len(active_connections) == 0:
+            start_time = time.time()
+            while len(active_connections) == 0 or (len(active_connections) < len(self.shared_arrs) and time.time() - start_time <= self.wait_time):
                 for shared_array in self.shared_arrs:
-                    if shared_array[0] == 1.0: # wait until the client has sent some data
+                    if shared_array[0] == 1.0 and len(active_connections) < len(self.shared_arrs): # wait until the client has sent some data
                         active_connections.append(shared_array)
                         start_index = 1
                         for input_name, (arr_len, _, arr_shape, _) in self.inputs_feed_config.items():
                             batched_input_feed[input_name].append(shared_array[start_index: start_index + arr_len].reshape(arr_shape))
                             start_index += arr_len
+
                     elif len(active_connections) >= 1:
                         inactivate_connections.append(shared_array)
 
-            for shared_array in inactivate_connections:
-                if shared_array[0] == 1.0:  # wait until the client has sent some data
-                    active_connections.append(shared_array)
-                    start_index = 1
-                    for input_name, (arr_len, _, arr_shape, _) in self.inputs_feed_config.items():
-                        batched_input_feed[input_name].append(
-                            shared_array[start_index: start_index + arr_len].reshape(arr_shape))
-                        start_index += arr_len
-
+            if len(active_connections) > len(self.shared_arrs):
+                raise ValueError("Too many samples in 1 batch sth went wrong")
             # implement sync by checking for inputs again
 
             for input_name, (_, transposition, shape, infer_dtype) in self.inputs_feed_config.items():
@@ -175,6 +174,11 @@ class Server:
                     batched_input_feed[input_name] = np.array(batched_input_feed[input_name], dtype=infer_dtype).transpose(transposition)
 
             # raise ValueError
+            # print(batched_input_feed.keys())
+            print(batched_input_feed["inputs"].shape)
+            print(batched_input_feed["input_state"].shape)
+            print(batched_input_feed["input_state_matrix"].shape)
+            # print(list(self.outputs_feed_info.keys()))
             batched_outputs = self.sess.run(list(self.outputs_feed_info.keys()), input_feed=batched_input_feed)
 
             # print(batched_outputs[1])
@@ -194,16 +198,17 @@ class Server:
                 flattened_outputs = np.concatenate([output[i].reshape((-1,)) for output in batched_outputs], dtype=np.float32)
                 shared_array[1:1 + len(flattened_outputs)] = flattened_outputs
                 shared_array[0] = 0.0 # reset it so that the session can pick it up
-def start_server(inputs_feed_info, outputs_feed_info, shms, providers, sess_options, file_path):
+def start_server(inputs_feed_info, outputs_feed_info, shms, providers, sess_options, file_path, per_process_wait_time=0.001):
 
     server = Server(inputs_feed_info,
                     outputs_feed_info,
                     shms,
                     providers,
                     sess_options,
-                    file_path
+                    file_path,
+                    per_process_wait_time
                     )
-
+    print("Started")
     server.start()
 def create_shared_memory(inputs_feed_info, outputs_feed_info, num_workers=os.cpu_count()):
     max_length_inputs = 1
