c37b37aec1ee1ba9dfb5b7496eddbd00cb6f60d1
TryHarder <brian8204001@gmail.com>
2025-01-07 17:15:41 -0500

    Changed the ignore number to -2 for both the policy loss and value loss.

--- Net/Custom_Loss.py
+++ Net/Custom_Loss.py
@@ -10,7 +10,7 @@ class Policy_Loss(tf.keras.Loss):
         loss = self.loss_fn(y_true, y_pred)
         # returns (batch_size,) as the shape for BCE loss
 
-        mask = tf.cast(y_true[:, 0] != -1, tf.float32) # if the first number of a policy is -1
+        mask = tf.cast(y_true[:, 0] != -2, tf.float32) # if the first number of a policy is -1
         # we mask out that sample of loss, this is because games have difference number of moves
         # thus we have to pad some samples to fit the maximum length of a game
         masked_loss = tf.reduce_mean(mask * loss)
@@ -26,7 +26,7 @@ class Value_Loss(tf.keras.Loss):
         loss = self.loss_fn(y_true, y_pred)
         # returns (batch_size,) as the shape for BCE loss
 
-        mask = tf.cast(y_true != -1, tf.float32) # if the first number of a policy is -1
+        mask = tf.cast(y_true != -2, tf.float32) # if the first number of a policy is -1
         # we mask out that sample of loss, this is because games have difference number of moves
         # thus we have to pad some samples to fit the maximum length of a game
         masked_loss = tf.reduce_mean(mask * loss)
