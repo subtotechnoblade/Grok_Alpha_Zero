24d630795f8bec5690afc6d391b86cb3e842d652
TryHarder <brian8204001@gmail.com>
2025-01-07 10:43:37 -0500

    Removed batched layers. Construct layers with the Batch wrapper.

--- Net/Batched_Net.py
+++ Net/Batched_Net.py
@@ -1,41 +1,18 @@
 import tensorflow as tf
 
-# This is the parallelized version of common layers such as Dense and Conv
+# This is the parallelized wrapper for common layers such as Dense and Conv
 # These are parallelized over the batch and number of moves dim
 # For example id the input is (batch_size, num_moves, board_shape) assuming board shape is a matrix
 # we have to matrix multiply num_moves meany times over the board matrix
 # But we also have to do all of that over the batch dimension
 # Thus these use the map_fn to repeat the process again
 # Ask Brian if you are confused
-class Batch_Dense(tf.keras.layers.Layer):
-    def __init__(self, units, use_bias=True, **kwargs):
+# Just wrap this class over Dense, Conv layers
+class Batch(tf.keras.layers.Layer):
+    def __init__(self, operation, **kwargs):
         super().__init__(**kwargs)
-        self.dense = tf.keras.layers.Dense(units, use_bias=use_bias)
+        self.operation = operation
+
     @tf.function
     def call(self, inputs):
-        return tf.vectorized_map(self.dense, inputs)
-
-class Batch_Conv1D(tf.keras.layers.Layer):
-    def __init__(self, filters, kernel_size=(1, 1), strides=(1, 1), use_bias=True, **kwargs):
-        super().__init__(**kwargs)
-        self.conv1D= tf.keras.layers.Conv1D(filters, kernel_size, strides, use_bias=use_bias)
-    @tf.function
-    def call(self, inputs):
-        return tf.vectorized_map(self.conv1D, inputs)
-
-
-class Batch_Conv2D(tf.keras.layers.Layer):
-    def __init__(self, filters, kernel_size=(1, 1), strides=(1, 1), padding="valid", use_bias=True, **kwargs):
-        super().__init__(**kwargs)
-        self.conv2D= tf.keras.layers.Conv2D(filters, kernel_size, strides, padding=padding, use_bias=use_bias)
-    @tf.function
-    def call(self, inputs):
-        return tf.vectorized_map(self.conv2D, inputs)
-
-class Batch_Conv3D(tf.keras.layers.Layer):
-    def __init__(self, filters, kernel_size=(1, 1, 1), strides=(1, 1, 1), padding="valid",use_bias=True, **kwargs):
-        super().__init__(**kwargs)
-        self.conv3D= tf.keras.layers.Conv3D(filters, kernel_size, strides, padding=padding, use_bias=use_bias)
-    @tf.function
-    def call(self, inputs):
-        return tf.vectorized_map(self.conv3D, inputs)
+        return tf.vectorized_map(self.operation, inputs)
--- Net/Batched_Net_Infer.py
+++ Net/Batched_Net_Infer.py
@@ -3,34 +3,10 @@ import tensorflow as tf
 # These are to be in replacement of the classes from Batched_Net
 # To create a inference model as training is parallelized
 # Ask Brian, if you are confused
-
-class Batch_Dense(tf.keras.layers.Layer):
-    def __init__(self, units, use_bias=True, **kwargs):
+class Batch(tf.keras.layers.Layer):
+    def __init__(self, operation, **kwargs):
         super().__init__(**kwargs)
-        self.dense = tf.keras.layers.Dense(units, use_bias=use_bias)
+        self.operation = operation
 
     def call(self, inputs):
-        return self.dense(inputs)
-
-class Batch_Conv1D(tf.keras.layers.Layer):
-    def __init__(self, filters, kernel_size=(1, 1), strides=(1, 1), use_bias=True, **kwargs):
-        super().__init__(**kwargs)
-        self.conv1D= tf.keras.layers.Conv1D(filters, kernel_size, strides, use_bias=use_bias)
-    def call(self, inputs):
-        return self.conv1D(inputs)
-
-class Batch_Conv2D(tf.keras.layers.Layer):
-    def __init__(self, filters, kernel_size=(1, 1), strides=(1, 1), padding="valid", use_bias=True, **kwargs):
-        super().__init__(**kwargs)
-        self.conv2D= tf.keras.layers.Conv2D(filters, kernel_size, strides, padding=padding, use_bias=use_bias)
-
-    def call(self, inputs):
-        return self.conv2D(inputs)
-
-class Batch_Conv3D(tf.keras.layers.Layer):
-    def __init__(self, filters, kernel_size=(1, 1, 1), strides=(1, 1, 1), padding="valid", use_bias=True, **kwargs):
-        super().__init__(**kwargs)
-        self.conv3D= tf.keras.layers.Conv3D(filters, kernel_size, strides, padding=padding, use_bias=use_bias)
-
-    def call(self, inputs):
-        return self.conv3D(inputs)
+        return self.operation(inputs)
