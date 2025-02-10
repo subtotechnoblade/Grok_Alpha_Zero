31f5fb52c578ab8d8a4e2c5b40f06c71a023d08a
atroee <adrianli9100@gmail.com>
2025-02-10 01:00:02 -0500

    made things

--- ChopsticksAdrian/Chopsticks.py
+++ ChopsticksAdrian/Chopsticks.py
@@ -1,3 +1,14 @@
-print("hello world")
-print("goodbye world")
-print("world")
+import numpy as np
+
+class Chopsticks:
+
+    def __init__(self):
+        self.board = np.array([1,2,3])
+
+
+
+if __name__ == "__main__":
+    from Game_Tester import Game_Tester
+    game_tester = Game_Tester(Chopsticks,)
+    game_tester.test()
+
