0c9567c4e8049de2cfcf99c03cfaafcd349229fb
atroee <adrianli9100@gmail.com>
2025-02-15 23:09:44 -0500

    defined variables(board, player, history, policy shape), and a few functions, working on legal actions

--- ChopsticksAdrian/Chopsticks.py
+++ ChopsticksAdrian/Chopsticks.py
@@ -3,7 +3,24 @@ import numpy as np
 class Chopsticks:
 
     def __init__(self):
-        self.board = np.array([1,2,3])
+        self.board = np.array([], dtype=np.int8)
+        self.current_player = -1 #p1 = -1, p2 = +1
+        self.action_history = []
+        self.policy_shape = (10,)
+
+    def get_current_player(self):
+        return self.current_player
+
+    def input_action(self):
+        #Takes "a b c" as input, returns [a b c]
+        action = map(int, input("Action:").split(" "))
+        return np.array(action)
+
+    def get_legal_actions(self):
+        LegalActions = [
+            [0, ]
+        ]
+
 
 
 
