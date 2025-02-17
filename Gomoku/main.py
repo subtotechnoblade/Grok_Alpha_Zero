4a30ea243f618fcfa7fee0c5a6843aa15d30409e
TryHarder <brian8204001@gmail.com>
2025-02-17 17:46:26 -0500

    Added testing before starting to train initiation process

--- Gomoku/main.py
+++ Gomoku/main.py
@@ -6,6 +6,8 @@ import tensorflow as tf
 import multiprocessing as mp
 
 from Gomoku import Gomoku, build_config, train_config
+from Game_Tester import Game_Tester
+
 from to_onnx import convert_to_onnx
 from Build_Model import build_model, build_model_infer
 from Build_Tensorrt import cache_tensorrt, get_speed
@@ -15,33 +17,44 @@ def validate_train_config(train_config):
     if train_config["total_generations"] <= 0:
         raise ValueError("Total generations can't less than or equal to zero")
 
-    if train_config["games_per_generation"] <= 0:
-        raise ValueError("Games per generation can't be less than or equal to zero")
+    if train_config["games_per_generation"] < 2:
+        raise ValueError("Games per generation can't be less than 2")
 
     if not train_config["use_gpu"] and train_config["use_tensorrt"]:
         raise ValueError("You must use the GPU for tensorrt")
-def initialize_training(): # This must be ran with a mp.Process
+def initialize_training(game_class): # This must be ran with a mp.Process
+    # test the game class before anything is done
+    Game_Tester(game_class).test()
+
+    print("\n*************Starting*************\n")
+
     os.makedirs("Grok_Zero_Train/0/", exist_ok=True)
 
     if os.path.exists("Grok_Zero_Train/0/model.keras") and not os.path.exists("Grok_Zero_Train/0/TRT_cache/model_ctx.onnx") and not os.path.exists("Grok_Zero_Train/0/model.onnx"):
         print("The starting folder is initialized incorrectly!")
-        print("Deleting and remaking the folder!")
+        print("Deleting and remaking the folder!\n")
         shutil.rmtree("Grok_Zero_Train/")
-        os.makedirs("Grok_Zero_Train/0/", exist_ok=True)
+        os.makedirs("Grok_Zero_Train/0/", exist_ok=False) # False should be fine as a precaution
     validate_train_config(train_config)
 
-    game = Gomoku()
+    game = game_class()
     def initialize_model(game, build_config):
         train_model = build_model(game.get_input_state().shape, game.policy_shape, build_config)
         train_model.save("Grok_Zero_Train/0/model.keras")
-    print("Initializing the model")
+    print("Initializing the model\n")
     p = mp.Process(target=initialize_model, args=(game, build_config))
     p.start()
     p.join()
 
 
     def initialize_onnx(build_config):
-        print("Converting tensorflow model to onnx")
+        physical_devices = tf.config.list_physical_devices('GPU')
+        try:
+            tf.config.experimental.set_memory_growth(physical_devices[0], True)
+        except:
+            # Invalid device or cannot modify virtual devices once initialized.
+            pass
+        print("Converting tensorflow model to onnx\n")
         embed_size, num_heads, num_layers = build_config["embed_size"], build_config["num_heads"], build_config["num_layers"]
         input_signature = [tf.TensorSpec((None, *game.get_input_state().shape), tf.float32, name="inputs"),
                            tf.TensorSpec((num_layers, 2, None, embed_size), tf.float32, name="input_state"),
@@ -51,11 +64,12 @@ def initialize_training(): # This must be ran with a mp.Process
         infer_model = build_model_infer(game.get_input_state().shape, game.policy_shape, build_config)
         infer_model.load_weights("Grok_Zero_Train/0/model.keras")
         convert_to_onnx(infer_model, input_signature, "Grok_Zero_Train/0/model.onnx")
-        print("Successfully converted to onnx")
+        print("Successfully converted to onnx\n")
 
     p = mp.Process(target=initialize_onnx, args=(build_config,))
     p.start()
     p.join()
+
 
     if train_config["use_gpu"] and train_config["use_tensorrt"]:
         p = mp.Process(target=cache_tensorrt, args=(game, build_config, train_config, "Grok_Zero_Train/", 0))
@@ -70,4 +84,4 @@ def initialize_training(): # This must be ran with a mp.Process
         file.create_dataset(f"max_moves", maxshape=(1,), dtype=np.int32, data=np.zeros(1,))
 
 if __name__ == "__main__":
-    initialize_training()
+    initialize_training(Gomoku)
