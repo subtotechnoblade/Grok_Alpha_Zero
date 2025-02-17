42560dacb154bc02f67903e03836c1b42a5e3fa4
TryHarder <brian8204001@gmail.com>
2025-02-17 17:38:25 -0500

    Updated to a more efficient way to pad the dataset

--- Pad_Dataset.py
+++ Pad_Dataset.py
@@ -26,11 +26,11 @@ class Pad_Dataset:
                 samples = (len(file.keys()) - 1) // 3
 
                 # use zeros for board
-                board_padding = np.expand_dims(np.zeros_like(file[f"boards_0"][0], dtype=file[f"boards_0"][0].dtype), 0 )
-
-                # use -2 as the padding value
-                policy_padding = np.expand_dims(np.ones_like(file[f"policies_0"][0], dtype=np.float32), 0) * -2.0
-                value_padding = np.expand_dims(np.ones_like(file[f"values_0"][0], dtype=np.float32), 0) * -2.0
+                # board_padding = np.expand_dims(np.zeros_like(file[f"boards_0"][0], dtype=file[f"boards_0"][0].dtype), 0 )
+                #
+                # # use -2 as the padding value
+                # policy_padding = np.expand_dims(np.ones_like(file[f"policies_0"][0], dtype=np.float32), 0) * -2.0
+                # value_padding = np.expand_dims(np.ones_like(file[f"values_0"][0], dtype=np.float32), 0) * -2.0
 
                 for game_id in range(samples):
                     if len(file[f"values_{game_id}"]) == self.max_moves:
@@ -38,20 +38,22 @@ class Pad_Dataset:
                         continue
                     game_len = len(file[f"values_{game_id}"])
 
-                    # resize which expand and retracts the array which deletes padding if self.max_moves is smalelr and vice versa
+                    # resize which expands
+                    # or retracts the array which deletes padding if self.max_moves is smaller and vice versa
                     file[f"boards_{game_id}"].resize(self.max_moves, axis=0)
                     file[f"policies_{game_id}"].resize(self.max_moves, axis=0)
                     file[f"values_{game_id}"].resize(self.max_moves, axis=0)
 
                     if game_len < self.max_moves:
                         # pad the boards
-                        file[f"boards_{game_id}"][game_len:] = np.repeat(board_padding, self.max_moves - game_len, axis=0)
-
+                        # file[f"boards_{game_id}"][game_len:] = np.repeat(board_padding, self.max_moves - game_len, axis=0)
+                        file[f"boards_{game_id}"][game_len:] = np.array(-2, dtype=file[f"boards_0"][0].dtype)
                         # pad the policies
-                        file[f"policies_{game_id}"][game_len:] = np.repeat(policy_padding, self.max_moves - game_len, axis=0)
+                        # file[f"policies_{game_id}"][game_len:] = np.repeat(policy_padding, self.max_moves - game_len, axis=0)
+                        file[f"policies_{game_id}"][game_len:] = -2.0
 
                         # pad the values
-                        file[f"values_{game_id}"][game_len:] = np.repeat(value_padding, self.max_moves - game_len, axis=0)
+                        file[f"values_{game_id}"][game_len:] = -2.0
 
 if __name__ == "__main__":
     pad = Pad_Dataset("Gomoku/Grok_Zero_Train/", 1)
@@ -59,8 +61,8 @@ if __name__ == "__main__":
     with h5.File("Gomoku/Grok_Zero_Train/0/Self_Play_Data.h5", "r") as file:
         print(len(file["values_0"]))
 
-    with h5.File("Gomoku/Grok_Zero_Train/1/Self_Play_Data.h5", "r") as file:
-        print(len(file["values_0"]))
+    # with h5.File("Gomoku/Grok_Zero_Train/1/Self_Play_Data.h5", "r") as file:
+    #     print(len(file["values_0"]))
 
 
 
