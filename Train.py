f3cbe6320afa8a53654b06db6a14ffd9a705e878
TryHarder <brian8204001@gmail.com>
2025-02-18 11:18:55 -0500

    Tested model training, should be fully workign

--- Train.py
+++ Train.py
@@ -1,13 +1,14 @@
 import tensorflow as tf
-from Gomoku.Gomoku import Gomoku, train_config
+from Gomoku.Gomoku import Gomoku, build_config, train_config
 
 from Dataloader import Create_Dataset
 from Net.Custom_Loss import Policy_Loss, Value_Loss
-def train(model, learning_rate, folder_path, train_config, generation):
+def train(model, learning_rate, folder_path, train_config):
     # assume that folder path is Grok_Zero_Train/0
     # generation = is the current generation, next generation in where the new model should be saved is generation + 1
+    generation = int(folder_path[-1])
     train_dataset, test_dataset = Create_Dataset(folder_path,
-                                                 num_previous_generations=train_config["num_previous_generation"],
+                                                 num_previous_generations=train_config["num_previous_generations"],
                                                  train_batch_size=train_config["train_batch_size"],
                                                  test_batch_size=train_config["test_batch_size"],
                                                  train_percent=train_config["train_percent"],
@@ -22,10 +23,18 @@ def train(model, learning_rate, folder_path, train_config, generation):
                   loss={"policy": Policy_Loss(), "value": Value_Loss()},
                   #Brian will probably create custom accuracies for this
                   # todo make accuracies class
+                  jit_compile=train_config["use_gpu"]
                   )
     model.fit(train_dataset,
               validation_data=test_dataset,
               epochs=train_config["train_epochs"])
+    model.save_weights(f"{folder_path[:-1]}/{generation + 1}/model.weights.h5")
+
+if __name__ == "__main__":
+    from Gomoku.Build_Model import build_model
+    game = Gomoku()
+    model = build_model(game.get_input_state().shape, game.policy_shape, build_config, train_config)
+    train(model, 5e-4, "Gomoku/Grok_Zero_Train/0", train_config)
 
 
 
