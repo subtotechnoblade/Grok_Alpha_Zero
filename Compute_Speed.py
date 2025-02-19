81e75ed978c0f252c714b42d3441744563f9c160
TryHarder <brian8204001@gmail.com>
2025-02-19 14:31:57 -0500

    Changed to use the correct shapes for trt engine

--- Compute_Speed.py
+++ Compute_Speed.py
@@ -1,38 +1,55 @@
 import time
 import numpy as np
 import onnxruntime as rt
-
+def convert_shape(shape):
+    assert len(shape) > 0
+    str_shape = ""
+    for dim in shape:
+        str_shape += f"{dim}x"
+    return str_shape[:-1]
 def compute_speed(game_class,
               build_config,
               train_config,
               folder_path,
               iterations=500):
-    game = game_class()
 
     embed_size = build_config["embed_size"]
     num_heads = build_config["num_heads"]
     num_layers = build_config["num_layers"]
 
-    max_shape = opt_shape = train_config["num_workers"]
-    providers = [
-        ('TensorrtExecutionProvider', {
-        "trt_engine_cache_enable":True,
-        "trt_dump_ep_context_model": True,
-        "trt_builder_optimization_level": 5,
-        "trt_auxiliary_streams": 0,
-        "trt_ep_context_file_path": f"{folder_path}/TRT_cache/",
 
-        "trt_profile_min_shapes": f"inputs:1x15x15,input_state:{num_layers}x2x1x{embed_size},input_state_matrix:{num_layers}x1x{num_heads}x{embed_size // num_heads}x{embed_size // num_heads}",
-        "trt_profile_max_shapes": f"inputs:{max_shape}x15x15,input_state:{num_layers}x2x{max_shape}x{embed_size},input_state_matrix:{num_layers}x{max_shape}x{num_heads}x{embed_size // num_heads}x{embed_size // num_heads}",
-        "trt_profile_opt_shapes": f"inputs:{opt_shape}x15x15,input_state:{num_layers}x2x{opt_shape}x{embed_size},input_state_matrix:{num_layers}x{opt_shape}x{num_heads}x{embed_size // num_heads}x{embed_size // num_heads}",
+    game = game_class()
+    board_shape = game.board.shape
+    str_board_shape = convert_shape(board_shape)
 
-        }),
-        'CUDAExecutionProvider',
-        'CPUExecutionProvider']
+    onnx_file_path = f"{folder_path}/model.onnx"
+    if train_config["use_gpu"]:
+        if train_config["use_tensorrt"]:
+            max_shape = train_config["num_workers"]
+            onnx_file_path = f"{folder_path}/TRT_cache/model_ctx.onnx"
+            providers = [
+                ('TensorrtExecutionProvider', {
+                    "trt_engine_cache_enable": True,
+                    "trt_dump_ep_context_model": True,
+                    "trt_builder_optimization_level": 5,
+                    "trt_auxiliary_streams": 0,
+
+                    "trt_ep_context_file_path": f"{folder_path}/TRT_cache/",
+                    "trt_profile_min_shapes": f"inputs:1x{str_board_shape},input_state:{num_layers}x2x1x{embed_size},input_state_matrix:{num_layers}x1x{num_heads}x{embed_size // num_heads}x{embed_size // num_heads}",
+                    "trt_profile_max_shapes": f"inputs:{max_shape}x{str_board_shape},input_state:{num_layers}x2x{max_shape}x{embed_size},input_state_matrix:{num_layers}x{max_shape}x{num_heads}x{embed_size // num_heads}x{embed_size // num_heads}",
+                    "trt_profile_opt_shapes": f"inputs:{max_shape}x{str_board_shape},input_state:{num_layers}x2x{max_shape}x{embed_size},input_state_matrix:{num_layers}x{max_shape}x{num_heads}x{embed_size // num_heads}x{embed_size // num_heads}",
+                }),
+                'CUDAExecutionProvider',
+                'CPUExecutionProvider']
+        else:
+            providers = [
+                'CUDAExecutionProvider',
+                'CPUExecutionProvider']
+    else:
+        providers = ['CPUExecutionProvider']
 
 
-    sess = rt.InferenceSession(f"{folder_path}/TRT_cache/model_ctx.onnx",
-                               providers=providers)
+    sess = rt.InferenceSession(onnx_file_path, providers=providers)
 
     dummy_inputs = np.random.uniform(-1, 2, (train_config["num_workers"], *game.get_input_state().shape)).astype(np.float32)
 
@@ -54,3 +71,8 @@ def compute_speed(game_class,
     time_taken = time.time() - s
     print(iterations)
     print(f"Took {time_taken / iterations} seconds per iteration at {iterations / time_taken:0.2f} it/s!")
+
+if __name__ == "__main__":
+    from TicTacToe.Tictactoe import TicTacToe, build_config, train_config
+
+    compute_speed(TicTacToe, build_config, train_config, "TicTacToe/Grok_Zero_Train/0")
