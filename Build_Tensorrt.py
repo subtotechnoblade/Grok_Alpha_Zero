5e7334977a41ba03916d28a0b38ae23ae00b3a0f
TryHarder <brian8204001@gmail.com>
2025-02-19 14:35:38 -0500

    Changed to use the correct shapes to build trt engine

--- Build_Tensorrt.py
+++ Build_Tensorrt.py
@@ -2,31 +2,40 @@ import time
 import numpy as np
 import onnxruntime as rt
 # This assumes that use_tensorrt is True in train config
+def convert_shape(shape):
+    assert len(shape) > 0
+    str_shape = ""
+    for dim in shape:
+        str_shape += f"{dim}x"
+    return str_shape[:-1]
 
 def cache_tensorrt(game_class,
                    build_config,
                    train_config,
                    folder_path,
                    warmup_iterations=10):
-    game = game_class()
     # uses the generation number to build the generation cache
     embed_size = build_config["embed_size"]
     num_heads = build_config["num_heads"]
     num_layers = build_config["num_layers"]
 
-    max_shape = opt_shape = train_config["num_workers"]
+    game = game_class()
+    board_shape = game.board.shape
+    str_board_shape = convert_shape(board_shape)
+    max_shape = train_config["num_workers"]
+
+
     providers = [
         ('TensorrtExecutionProvider', {
-        "trt_engine_cache_enable":True,
-        "trt_dump_ep_context_model": True,
-        "trt_builder_optimization_level": 5,
-        "trt_auxiliary_streams": 0,
-        "trt_ep_context_file_path": f"{folder_path}/TRT_cache/",
+            "trt_engine_cache_enable": True,
+            "trt_dump_ep_context_model": True,
+            "trt_builder_optimization_level": 5,
+            "trt_auxiliary_streams": 0,
 
-            "trt_profile_min_shapes": f"inputs:1x15x15,input_state:{num_layers}x2x1x{embed_size},input_state_matrix:{num_layers}x1x{num_heads}x{embed_size // num_heads}x{embed_size // num_heads}",
-            "trt_profile_max_shapes": f"inputs:{max_shape}x15x15,input_state:{num_layers}x2x{max_shape}x{embed_size},input_state_matrix:{num_layers}x{max_shape}x{num_heads}x{embed_size // num_heads}x{embed_size // num_heads}",
-            "trt_profile_opt_shapes": f"inputs:{opt_shape}x15x15,input_state:{num_layers}x2x{opt_shape}x{embed_size},input_state_matrix:{num_layers}x{opt_shape}x{num_heads}x{embed_size // num_heads}x{embed_size // num_heads}",
-
+            "trt_ep_context_file_path": f"{folder_path}/TRT_cache/",
+            "trt_profile_min_shapes": f"inputs:1x{str_board_shape},input_state:{num_layers}x2x1x{embed_size},input_state_matrix:{num_layers}x1x{num_heads}x{embed_size // num_heads}x{embed_size // num_heads}",
+            "trt_profile_max_shapes": f"inputs:{max_shape}x{str_board_shape},input_state:{num_layers}x2x{max_shape}x{embed_size},input_state_matrix:{num_layers}x{max_shape}x{num_heads}x{embed_size // num_heads}x{embed_size // num_heads}",
+            "trt_profile_opt_shapes": f"inputs:{max_shape}x{str_board_shape},input_state:{num_layers}x2x{max_shape}x{embed_size},input_state_matrix:{num_layers}x{max_shape}x{num_heads}x{embed_size // num_heads}x{embed_size // num_heads}",
         }),
         'CUDAExecutionProvider',
         'CPUExecutionProvider']
