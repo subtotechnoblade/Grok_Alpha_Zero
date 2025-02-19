8f2fc131f54817726619af4e23dfb68f5115d885
TryHarder <brian8204001@gmail.com>
2025-02-19 11:40:23 -0500

    Validated Train.py, fully working

--- Train.py
+++ Train.py
@@ -2,9 +2,8 @@ import tensorflow as tf
 
 from Dataloader import Create_Dataset
 from Net.Custom_Loss import Policy_Loss, Value_Loss
-def train(model, learning_rate, train_config, save_folder_path):
+def train(model, learning_rate, train_config, folder_path, save_folder_path):
     # assume that save_folder path is Grok_Zero_Train/current_generation + 1
-
     train_dataset, test_dataset = Create_Dataset(folder_path,
                                                  num_previous_generations=train_config["num_previous_generations"],
                                                  train_batch_size=train_config["train_batch_size"],
@@ -14,10 +13,19 @@ def train(model, learning_rate, train_config, save_folder_path):
                                                  test_percent=train_config["test_percent"],
                                                  test_decay=train_config["test_decay"],)
 
-    model.compile(optimizer=tf.keras.optimizers.Nadam(learning_rate=learning_rate,
-                                                      beta_1=train_config["beta_1"],
-                                                      beta_2=train_config["beta_2"],
-                                                      weight_decay=1e-4),
+    kwargs = {"learning_rate": learning_rate,
+              "beta_1": train_config["beta_1"],
+              "beta_2": train_config["beta_2"],
+              "weight_decay": 1e-4
+              }
+    if train_config["optimizer"].lower() == "adam":
+        optimizer = tf.keras.optimizers.Adam(**kwargs)
+    elif train_config["optimizer"].lower() == "adamw":
+        optimizer = tf.keras.optimizers.AdamW(**kwargs)
+    elif train_config["optimizer"].lower() == "nadam":
+        optimizer = tf.keras.optimizers.Nadam(**kwargs)
+
+    model.compile(optimizer=optimizer,
                   loss={"policy": Policy_Loss(), "value": Value_Loss()},
                   #Brian will probably create custom accuracies for this
                   # todo make accuracies class
@@ -26,10 +34,11 @@ def train(model, learning_rate, train_config, save_folder_path):
     model.fit(train_dataset,
               validation_data=test_dataset,
               epochs=train_config["train_epochs"])
-    model.save_weights(f"{save_folder_path}/model.weights.h5")
+    if save_folder_path:
+        model.save_weights(f"{save_folder_path}/model.weights.h5")
 
 if __name__ == "__main__":
-    folder_path = "TicTacToe/Grok_Zero_Train/1"
+    folder_path = "TicTacToe/Grok_Zero_Train/0"
     from Gomoku.Gomoku import Gomoku, build_config, train_config
     from Gomoku.Build_Model import build_model
 
@@ -39,7 +48,7 @@ if __name__ == "__main__":
     # game = Gomoku()
     game = TicTacToe()
     model = build_model(game.get_input_state().shape, game.policy_shape, build_config)
-    train(model, 5e-4, folder_path, train_config)
+    train(model, 5e-4, train_config, folder_path, False)
 
 
 
