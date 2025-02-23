4ddc2b64e63bde72fcc53354e2c30336588f58a0
TryHarder <brian8204001@gmail.com>
2025-02-23 16:36:51 -0500

    Removed weight decay, as overfitting isn't an issue anymore

--- Train.py
+++ Train.py
@@ -2,9 +2,9 @@ import tensorflow as tf
 
 from Dataloader import Create_Dataset
 from Net.Custom_Loss import Policy_Loss, Value_Loss, KLD
-def train(model, learning_rate, train_config, folder_path, save_folder_path):
+def train(model, learning_rate, train_config, parent_path):
     # assume that save_folder path is Grok_Zero_Train/current_generation + 1
-    train_dataset, test_dataset = Create_Dataset(folder_path,
+    train_dataset, test_dataset = Create_Dataset(parent_path,
                                                  num_previous_generations=train_config["num_previous_generations"],
                                                  train_batch_size=train_config["train_batch_size"],
                                                  test_batch_size=train_config["test_batch_size"],
@@ -16,7 +16,7 @@ def train(model, learning_rate, train_config, folder_path, save_folder_path):
     kwargs = {"learning_rate": learning_rate,
               "beta_1": train_config["beta_1"],
               "beta_2": train_config["beta_2"],
-              "weight_decay": 1e-4
+              # "weight_decay": 1e-4
               }
     if train_config["optimizer"].lower() == "adam":
         optimizer = tf.keras.optimizers.Adam(**kwargs)
@@ -34,11 +34,11 @@ def train(model, learning_rate, train_config, folder_path, save_folder_path):
     model.fit(train_dataset,
               validation_data=test_dataset,
               epochs=train_config["train_epochs"])
-    if save_folder_path:
-        model.save_weights(f"{save_folder_path}/model.weights.h5")
+    return model
 
 if __name__ == "__main__":
-    folder_path = "TicTacToe/Grok_Zero_Train/0"
+    from pathlib import Path
+    folder_path = "TicTacToe/Grok_Zero_Train/1"
     from Gomoku.Gomoku import Gomoku, build_config, train_config
     from Gomoku.Build_Model import build_model
 
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     # game = Gomoku()
     game = TicTacToe()
     model = build_model(game.get_input_state().shape, game.policy_shape, build_config)
-    train(model, 5e-4, train_config, folder_path, False)
+    train(model, 5e-4, train_config,  str(Path(folder_path).parent), False)
 
 
 
