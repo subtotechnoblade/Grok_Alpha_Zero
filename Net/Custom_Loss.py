6965fb36baebf592ea70dcc0fd6cb2897bd2ae7f
TryHarder <brian8204001@gmail.com>
2025-02-25 17:18:58 -0500

    Cleanup

--- Net/Custom_Loss.py
+++ Net/Custom_Loss.py
@@ -10,7 +10,7 @@ class Stable_Categorical_Crossentropy(tf.keras.Loss):
         return loss
 
 class Stable_Categorical_Focal_Crossentropy(tf.keras.Loss):
-    def __init__(self, alpha=0.25, gamma=2.0, **kwargs):
+    def __init__(self, alpha=0.5, gamma=2.0, **kwargs):
         super().__init__(**kwargs)
 
         self.alpha = alpha
@@ -30,7 +30,7 @@ class Policy_Loss(tf.keras.Loss):
         loss = self.loss_fn(y_true, y_pred)
         # returns (batch_size,) as the shape for BCE loss
 
-        mask = tf.cast(y_true[:, :, 0] != -2, tf.float32) # if the first number of a policy is -2
+        mask = tf.cast(y_true[:, :, 0] != -2.0, tf.float32) # if the first number of a policy is -2
         # we mask out that sample of loss, this is because games have difference number of moves
         # thus we have to pad some samples to fit the maximum length of a game
         return tf.reduce_mean(tf.reduce_sum(mask * loss, axis=-1) / tf.reduce_sum(mask, axis=-1))
@@ -44,7 +44,7 @@ class Value_Loss(tf.keras.Loss):
         # Note that y_true is a vector [batch_size, policy_shape]
         loss = self.loss_fn(y_true, y_pred)
         # returns (batch_size,) as the shape for BCE loss
-        mask = tf.cast(y_true[:, :, 0] != -2, tf.float32) # if the first number of a policy is -1
+        mask = tf.cast(y_true[:, :, 0] != -2.0, tf.float32) # if the first number of a policy is -1
         # we mask out that sample of loss, this is because games have difference number of moves
         # thus we have to pad some samples to fit the maximum length of a game
         return tf.reduce_mean(tf.reduce_sum(mask * loss, axis=-1) / tf.reduce_sum(mask, axis=-1))
@@ -57,7 +57,7 @@ class KLD(tf.keras.Loss):
 
     def call(self, y_true, y_pred):
         loss = self.loss_fn(y_true, y_pred)
-        mask = tf.cast(y_true[:, :, 0] != -2, tf.float32)
+        mask = tf.cast(y_true[:, :, 0] != -2.0, tf.float32)
         masked_loss = tf.reduce_mean(tf.reduce_sum(mask * loss, axis=-1) / tf.reduce_sum(mask, -1))
         return masked_loss
 
