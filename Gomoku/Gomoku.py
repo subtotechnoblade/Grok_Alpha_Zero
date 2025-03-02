670f8e3eca9f66f2b38b2fce330cf78237455cfa
TryHarder <brian8204001@gmail.com>
2025-03-02 11:32:23 -0500

    Increased model param count

--- Gomoku/Gomoku.py
+++ Gomoku/Gomoku.py
@@ -6,7 +6,7 @@ build_config = {"embed_size": 256, # this is the vector for RWKV
                 "num_heads": 2, # this must be a factor of embed_size or else an error will be raised
                 "token_shift_hidden_dim": 32, # this is in the RWKV paper
                 "hidden_size": None, # None uses the default 3.5 * embed , factor for upscaling in channel mix
-                "num_filters": 96,
+                "num_filters": 128,
                 "num_layers": 0, # This is the total amount of RWKV layers in the model that are used
                 "num_resnet_layers": 3, # This is the total amount of RWKV layers in the model that are used
 
@@ -21,8 +21,8 @@ train_config = {
     # a generation is defined by a round of self play, padding the dataset, model training, converting to onnx
 
     # Self Play variables
-    "games_per_generation": 10, # amount of self play games until we re train the network
-    "max_actions": 100, # Note that this should be
+    "games_per_generation": 1000, # amount of self play games until we re train the network
+    "max_actions": 150, # Note that this should be
     "num_explore_actions_first": 3,  # A good rule of thumb is how long the opening should be for player -1
     "num_explore_actions_second": 2, # Since player 1 is always at a disadvantage, we explore less and attempt to play better moves
 
@@ -31,32 +31,32 @@ train_config = {
     # change this to False to use CUDAExecutionProvider
     "use_inference_server": True, # if an extremely large model is used, because of memory constraints, set this to True
     "max_cache_depth": 2,  # maximum depth in the search of the neural networks outputs we should cache
-    "num_workers": 6, # Number of multiprocessing workers used to self play
+    "num_workers": 8, # Number of multiprocessing workers used to self play
 
     # MCTS variables
-    "MCTS_iteration_limit": 500, # The number of iterations MCTS runs for. Should be 2 to 10x the number of starting legal moves
+    "MCTS_iteration_limit": 600, # The number of iterations MCTS runs for. Should be 2 to 10x the number of starting legal moves
     # True defaults to iteration_limit = 3 * len(starting legal actions)
     "MCTS_time_limit": None, # Not recommended to use for training, True defaults to 30 seconds
     "c_puct_init": 1.25, # (shouldn't change) Exploration constant lower -> exploitation, higher -> exploration
-    "dirichlet_alpha": 0.3, # should be around (10 / average moves per game)
+    "dirichlet_alpha": 0.333, # should be around (10 / average moves per game)
 
-    "opening_actions": [[[7, 7], 0.6], [[6, 6], 0.05], [[7, 6], 0.05], [[8, 6], 0.05], [[6, 7], 0.05], [[8, 7], 0.05], [[6, 8], 0.05], [[7, 8], 0.05], [[8, 8], 0.05]], # starting first move in the format [[action1, prob0], [action1, prob1], ...],
+    "opening_actions": [[[7, 7], 0.45], [[6, 6], 0.05], [[7, 6], 0.05], [[8, 6], 0.05], [[6, 7], 0.05], [[8, 7], 0.05], [[6, 8], 0.05], [[7, 8], 0.05], [[8, 8], 0.05]], # starting first move in the format [[action1, prob0], [action1, prob1], ...],
     # if prob doesn't add up to 1, then the remaining prob is for the MCTS move
 
-    "num_previous_generations": 3, # The previous generation's data that will be used in training
+    "num_previous_generations": 4, # The previous generation's data that will be used in training
     "train_percent": 1.0, # The percent used for training after the test set is taken
-    "train_decay": 0.75, # The decay rate for previous generations of data previous_train_percent = current_train_percent * train_decay
+    "train_decay": 0.9, # The decay rate for previous generations of data previous_train_percent = current_train_percent * train_decay
     "test_percent": 0.1, # The percent of a dataset that will be used for validation
-    "test_decay": 0.75, # The decay rate for previous generations of data previous_test_percent = current_test_percent * test_decay
+    "test_decay": 0.9, # The decay rate for previous generations of data previous_test_percent = current_test_percent * test_decay
 
-    "train_batch_size": 8, # The number of samples in a batch for training in parallel
+    "train_batch_size": 4, # The number of samples in a batch for training in parallel
     "test_batch_size": 4, # If none, then train_batch_size will be used for the test batch size
     "gradient_accumulation_steps": 2,
-    "learning_rate": 4e-4, # Depending on how many RWKV blocks you use. Recommended to be between 1e-3 to 5e-4
+    "learning_rate": 7e-4, # Depending on how many RWKV blocks you use. Recommended to be between 1e-3 to 5e-4
     "decay_lr_after": 20,  # When the n generations pass,... learning rate will be decreased by lr_decay
-    "lr_decay": 0.5,  # multiplies this to learning rate every decay_lr_after
+    "lr_decay": 0.75,  # multiplies this to learning rate every decay_lr_after
     "beta_1": 0.9, # DO NOT TOUCH unless you know what you are doing
-    "beta_2": 0.985, # DO NOT TOUCH. This determines whether it groks or not. Hovers between 0.985 to 0.995
+    "beta_2": 0.99, # DO NOT TOUCH. This determines whether it groks or not. Hovers between 0.985 to 0.995
     "optimizer": "Nadam",  # optimizer options are ["Adam", "AdamW", "Nadam"]
     "train_epochs": 15, # The number of epochs for training
 }
@@ -145,6 +145,41 @@ class Gomoku:
     def get_input_state_MCTS(board: np.array, current_player: int, action_history: np.array) -> np.array:
         current_player_plane = np.expand_dims(np.ones_like(board) * current_player, -1)
         return np.concatenate((current_player_plane, np.expand_dims(board,-1)), axis=-1)
+
+    # @staticmethod
+    # @njit(cache=True, nogil=True, fastmath=True)
+    # def get_input_state_MCTS(board, current_player, input_move_history: np.array, num_previous_moves: int = 3):
+    #     WIDTH, HEIGHT= 15, 15
+    #     NP_DTYPE = np.int16
+    #     SHAPE = (1, 3 + 1, HEIGHT, WIDTH, 1)
+    #     inference_board = []
+    #     board = [[0 for _ in range(WIDTH)] for _ in range(HEIGHT)]
+    #
+    #     for i, (x, y) in enumerate(input_move_history[1:]):
+    #         if i % 2 == 0:
+    #             board[y][x] = -1
+    #         else:
+    #             board[y][x] = 1
+    #         inference_board.append(
+    #             [[*row] for row in board])  # the fastest way to deepcopy knowing the structure of the list
+    #
+    #     current_player = 1 if input_move_history.shape[0] % 2 == 0 else -1
+    #     num_placed_moves = len(inference_board)
+    #     if num_placed_moves >= num_previous_moves:
+    #         inference_board = np.array(inference_board, dtype=NP_DTYPE)[-num_previous_moves:]
+    #         return np.concatenate(
+    #             (
+    #             np.full(shape=(1, HEIGHT, WIDTH), fill_value=current_player, dtype=NP_DTYPE), inference_board)).reshape(
+    #             SHAPE)
+    #     elif num_placed_moves < num_previous_moves and num_placed_moves != 0:
+    #         inference_board = np.array(inference_board, dtype=np.int8)
+    #         return np.concatenate((np.full(shape=(1, HEIGHT, WIDTH), fill_value=current_player, dtype=NP_DTYPE),
+    #                                np.zeros(shape=(num_previous_moves - num_placed_moves, HEIGHT, WIDTH),
+    #                                         dtype=NP_DTYPE),
+    #                                inference_board)).reshape(SHAPE)
+    #     else:
+    #         return np.concatenate((np.full(shape=(1, HEIGHT, WIDTH), fill_value=current_player, dtype=NP_DTYPE),
+    #                                np.zeros(shape=(num_previous_moves, HEIGHT, WIDTH), dtype=NP_DTYPE))).reshape(SHAPE)
 
     def check_win(self, ) -> int:
         """
