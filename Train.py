7ede21b877062c16c0bf4a45aa504e7e861a6315
TryHarder <brian8204001@gmail.com>
2025-03-03 14:23:41 -0500

    Removed support for time parallel

--- Train.py
+++ Train.py
@@ -1,33 +1,14 @@
 import tensorflow as tf
 
 from Net.Custom_Loss import Policy_Loss, Value_Loss, KLD
-from Net_Time_Parallel.Custom_Loss_Time_Parallel import Policy_Loss_Time_Parallel, Value_Loss_Time_Parallel, KLD_Time_Parallel
-def train(train_dataloader, test_dataloader, model, learning_rate, train_config, parent_path):
+def train(train_dataloader, test_dataloader, model, learning_rate, train_config):
     # assume that save_folder path is Grok_Zero_Train/current_generation + 1
-    # train_dataset, test_dataset = Create_Dataset(parent_path,
-    #                                              num_previous_generations=train_config["num_previous_generations"],
-    #                                              train_batch_size=train_config["train_batch_size"],
-    #                                              test_batch_size=train_config["test_batch_size"],
-    #                                              train_percent=train_config["train_percent"],
-    #                                              train_decay=train_config["train_decay"],
-    #                                              test_percent=train_config["test_percent"],
-    #                                              test_decay=train_config["test_decay"],)
-
     kwargs = {"learning_rate": learning_rate,
               "beta_1": train_config["beta_1"],
               "beta_2": train_config["beta_2"],
               # "weight_decay": 1e-4,
               "gradient_accumulation_steps": train_config["gradient_accumulation_steps"],
               }
-
-    if train_config["use_time_parallel"]:
-        policy_loss = Policy_Loss_Time_Parallel()
-        value_loss = Value_Loss_Time_Parallel()
-        kld = KLD_Time_Parallel()
-    else:
-        policy_loss = Policy_Loss()
-        value_loss = Value_Loss()
-        kld = KLD()
 
     if train_config["optimizer"].lower() == "adam":
         optimizer = tf.keras.optimizers.Adam(**kwargs)
@@ -37,8 +18,8 @@ def train(train_dataloader, test_dataloader, model, learning_rate, train_config,
         optimizer = tf.keras.optimizers.Nadam(**kwargs)
 
     model.compile(optimizer=optimizer,
-                  loss={"policy": policy_loss, "value": value_loss},
-                  metrics={"policy": kld}
+                  loss={"policy": Policy_Loss(), "value": Value_Loss()},
+                  metrics={"policy": KLD()}
                   )
     model.fit(train_dataloader,
               validation_data=test_dataloader,
