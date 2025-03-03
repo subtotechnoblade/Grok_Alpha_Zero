db1560b2823148e6cc93fd1f2d276d97c3b05b76
TryHarder <brian8204001@gmail.com>
2025-03-02 22:03:06 -0500

    Changed Pad_Dataset.py back to use max_actions as we can limit max_actions in train_config

--- Pad_Dataset.py
+++ Pad_Dataset.py
@@ -1,7 +1,10 @@
 import h5py as h5
 import numpy as np
+from tqdm import tqdm
+
 from glob import glob
 from pathlib import Path
+
 class Pad_Dataset:
     def __init__(self, folder_path, num_previous_generations):
         outer_folder_path = Path(folder_path).resolve().parent
@@ -13,9 +16,8 @@ class Pad_Dataset:
         for path in self.file_paths:
             with h5.File(path) as file:
                 max_actions, total_actions, num_unaugmented_games, _, _, _ = file["game_stats"][:]
-                average_actions = round(((total_actions / num_unaugmented_games) + max_actions) / 2)
-                if average_actions > self.max_actions:
-                    self.max_actions = average_actions
+                if max_actions > self.max_actions:
+                    self.max_actions = max_actions
 
     def pad_dataset(self):
         print("Padding the datasets!")
@@ -23,12 +25,12 @@ class Pad_Dataset:
             with h5.File(file_path, mode="a", libver="latest") as file:
                 max_actions = file["game_stats"][0]
 
-                if max_actions > self.max_actions:
-                    raise ValueError("self.max_actions is smaller that this dataset's max moves. This shouldn't be the case!")
+                # if max_actions > self.max_actions:
+                #     raise ValueError("self.max_actions is smaller that this dataset's max moves. This shouldn't be the case!")
 
                 samples = (len(file.keys()) - 1) // 3
 
-                for game_id in range(samples):
+                for game_id in tqdm(range(samples), desc="Padding"):
                     if len(file[f"values_{game_id}"]) == self.max_actions:
                         # if the game satisfies self.max_actions then don't do anything
                         continue
@@ -50,13 +52,16 @@ class Pad_Dataset:
                         # pad the values
                         file[f"values_{game_id}"][game_len:] = -2.0
 
+
 if __name__ == "__main__":
-    folder_path = "TicTacToe/Grok_Zero_Train/0/"
-    pad = Pad_Dataset(folder_path, 1)
-    pad.pad_dataset()
-    with h5.File(f"{folder_path}/Self_Play_Data.h5", "r") as file:
-        print(len(file["values_0"]))
-        print(file["game_stats"][2])
+    folder_path = "Gomoku/Grok_Zero_Train/0/"
+
+
+    # pad = Pad_Dataset(folder_path, 1)
+    # pad.pad_dataset()
+    # with h5.File(f"{folder_path}/Self_Play_Data.h5", "r") as file:
+    #     print(len(file["values_0"]))
+    #     print(file["game_stats"][2])
 
     # with h5.File("Gomoku/Grok_Zero_Train/1/Self_Play_Data.h5", "r") as file:
     #     print(len(file["values_0"]))
