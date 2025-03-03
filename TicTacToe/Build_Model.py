a791d4f1112c816b72423807d3cc437feec0c6fa
TryHarder <brian8204001@gmail.com>
2025-03-03 17:38:29 -0500

    Final small model

--- TicTacToe/Build_Model.py
+++ TicTacToe/Build_Model.py
@@ -13,18 +13,19 @@ def build_model(input_shape, policy_shape, build_config):
     # input shape should be (3, 3)
     inputs = tf.keras.layers.Input(batch_shape=(None, *input_shape), name="inputs") # the name must be "inputs"
 
-    x = tf.keras.layers.Conv2D(256, (5, 5), padding="same")(inputs)
+    x = tf.keras.layers.Conv2D(128, (5, 5), padding="same")(inputs)
+    x = tf.keras.layers.Conv2D(64, (3, 3), padding="same")(x)
 
-    for _ in range(2):
-        x = ResNet_Conv2D(128, (3, 3), activation="relu")(x)
-        x = ResNet_Identity2D(128, (3, 3), activation="relu")(x)
+    # for _ in range(2):
+    #     x = ResNet_Conv2D(64, (3, 3), activation="relu")(x)
+    #     x = ResNet_Identity2D(64, (3, 3), activation="relu")(x)
 
-    policy = tf.keras.layers.BatchNormalization()(x)
-    policy = tf.keras.layers.Conv2D(2, (1, 1), padding="valid")(policy)
+    # policy = tf.keras.layers.BatchNormalization()(x)
+    policy = tf.keras.layers.Conv2D(2, (1, 1), padding="valid")(x)
     policy = tf.keras.layers.Reshape((-1,))(policy)
-    policy = tf.keras.layers.Dense(256)(policy)
-    policy = tf.keras.layers.Activation("relu")(policy)
     policy = tf.keras.layers.Dense(128)(policy)
+    policy = tf.keras.layers.Activation("relu")(policy)
+    policy = tf.keras.layers.Dense(64)(policy)
 
     policy = tf.keras.layers.Dense(policy_shape[0])(policy)
 
@@ -33,13 +34,13 @@ def build_model(input_shape, policy_shape, build_config):
     else:
         policy = tf.keras.layers.Activation("softmax", name="policy")(policy)  # MUST NAME THIS "policy"
 
-    value = tf.keras.layers.BatchNormalization()(x)
-    value = tf.keras.layers.Conv2D(2, (1, 1), padding="valid")(value)
+    # value = tf.keras.layers.BatchNormalization()(x)
+    value = tf.keras.layers.Conv2D(2, (1, 1), padding="valid")(x)
     value = tf.keras.layers.Reshape((-1,))(value)
 
-    value = tf.keras.layers.Dense(256)(value)
-    value = tf.keras.layers.Activation("relu")(value)
     value = tf.keras.layers.Dense(128)(value)
+    value = tf.keras.layers.Dense(64)(value)
+    value = tf.keras.layers.Activation("relu")(value)
     value = tf.keras.layers.Dense(1)(value)
     value = tf.keras.layers.Activation("tanh", name="value")(value) # MUST NAME THIS "value"
 
