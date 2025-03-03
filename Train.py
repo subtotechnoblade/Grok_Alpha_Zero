93eae285387d6727820fba2c937c7363418cae14
TryHarder <brian8204001@gmail.com>
2025-03-02 22:31:09 -0500

    Removed dependence on creating a train and test dataloader, also added option to specify time parallel losses

--- Train.py
+++ Train.py
@@ -1,24 +1,34 @@
 import tensorflow as tf
 
-from Dataloader import Create_Dataset
 from Net.Custom_Loss import Policy_Loss, Value_Loss, KLD
-def train(model, learning_rate, train_config, parent_path):
+from Net.Custom_Loss_Time_Parallel import Policy_Loss_Time_Parallel, Value_Loss_Time_Parallel, KLD_Time_Parallel
+def train(train_dataloader, test_dataloader, model, learning_rate, train_config, parent_path):
     # assume that save_folder path is Grok_Zero_Train/current_generation + 1
-    train_dataset, test_dataset = Create_Dataset(parent_path,
-                                                 num_previous_generations=train_config["num_previous_generations"],
-                                                 train_batch_size=train_config["train_batch_size"],
-                                                 test_batch_size=train_config["test_batch_size"],
-                                                 train_percent=train_config["train_percent"],
-                                                 train_decay=train_config["train_decay"],
-                                                 test_percent=train_config["test_percent"],
-                                                 test_decay=train_config["test_decay"],)
+    # train_dataset, test_dataset = Create_Dataset(parent_path,
+    #                                              num_previous_generations=train_config["num_previous_generations"],
+    #                                              train_batch_size=train_config["train_batch_size"],
+    #                                              test_batch_size=train_config["test_batch_size"],
+    #                                              train_percent=train_config["train_percent"],
+    #                                              train_decay=train_config["train_decay"],
+    #                                              test_percent=train_config["test_percent"],
+    #                                              test_decay=train_config["test_decay"],)
 
     kwargs = {"learning_rate": learning_rate,
               "beta_1": train_config["beta_1"],
               "beta_2": train_config["beta_2"],
-              "weight_decay": 1e-4,
+              # "weight_decay": 1e-4,
               "gradient_accumulation_steps": train_config["gradient_accumulation_steps"],
               }
+
+    if train_config["use_time_parallel"]:
+        policy_loss = Policy_Loss_Time_Parallel()
+        value_loss = Value_Loss_Time_Parallel()
+        kld = KLD_Time_Parallel()
+    else:
+        policy_loss = Policy_Loss()
+        value_loss = Value_Loss()
+        kld = KLD()
+
     if train_config["optimizer"].lower() == "adam":
         optimizer = tf.keras.optimizers.Adam(**kwargs)
     elif train_config["optimizer"].lower() == "adamw":
@@ -26,14 +36,12 @@ def train(model, learning_rate, train_config, parent_path):
     elif train_config["optimizer"].lower() == "nadam":
         optimizer = tf.keras.optimizers.Nadam(**kwargs)
 
-
-
     model.compile(optimizer=optimizer,
-                  loss={"policy": Policy_Loss(), "value": Value_Loss()},
-                  metrics={"policy": KLD()}
+                  loss={"policy": policy_loss, "value": value_loss},
+                  metrics={"policy": kld}
                   )
-    model.fit(train_dataset,
-              validation_data=test_dataset,
+    model.fit(train_dataloader,
+              validation_data=test_dataloader,
               epochs=train_config["train_epochs"])
     return model
 
