5d3d8b44a9ea34598b148deb22e9f4431adaf77e
TryHarder <brian8204001@gmail.com>
2025-03-22 21:49:39 -0400

    Formatting

--- Net/SE/SE_Block.py
+++ Net/SE/SE_Block.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+
 class SE_Block(tf.keras.layers.Layer):
     def __init__(self, num_filers, broadcast_shape, ratio=2, **kwargs):
         super().__init__(**kwargs)
@@ -10,6 +11,7 @@ class SE_Block(tf.keras.layers.Layer):
         self.squeeze = tf.keras.layers.GlobalAveragePooling3D()
         self.dense1 = tf.keras.layers.Dense(units=self.num_filters / self.ratio)
         self.dense2 = tf.keras.layers.Dense(units=self.num_filters)
+
     def call(self, inputs):
         squeeze = self.squeeze(inputs)
         excitation = self.dense1(squeeze)
