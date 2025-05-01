7fcf5137d63990e1cebb5617cb9b874f4a0c7e77
TryHarder <brian8204001@gmail.com>
2025-05-01 12:26:07 -0400

    Used the taylor expansion of the grok paper rather than the formal math one

---
+++ Net/Taylor_Softmax.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+
+class Taylor_Softmax(tf.keras.layers.Layer):
+    def taylor_e(self, x):
+        return (1 + x + x ** 2) / 2
+
+    def call(self, inputs):
+        batch_min = tf.expand_dims(tf.reduce_min(inputs, -1), -1)
+        taylor_exp = self.taylor_e(inputs - batch_min)
+        return taylor_exp / tf.reduce_sum(taylor_exp)
+
+if __name__ == "__main__":
+    import numpy as np
+
+    # x = np.array([[1, 2, 3], [2, 3, 4]], dtype=np.float32)
+    x = np.ones((1, 9, 2), dtype=np.float32)
+    fn = Taylor_Softmax()
+    print(fn(x))
