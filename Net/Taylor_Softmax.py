Fixed incorrect dim summing for denum

Net/Taylor_Softmax.py
@@ -8,12 +8,12 @@ class Taylor_Softmax(tf.keras.layers.Layer):
     def call(self, inputs):
         batch_min = tf.expand_dims(tf.reduce_min(inputs, -1), -1)
         taylor_exp = self.taylor_e(inputs - batch_min)
-        return taylor_exp / tf.reduce_sum(taylor_exp)
+        return taylor_exp / tf.expand_dims(tf.reduce_sum(taylor_exp, -1), -1)
 
 if __name__ == "__main__":
     import numpy as np
 
     # x = np.array([[1, 2, 3], [2, 3, 4]], dtype=np.float32)
-    x = np.ones((1, 9, 2), dtype=np.float32)
+    x = np.ones((2, 9), dtype=np.float32)
     fn = Taylor_Softmax()
     print(fn(x))