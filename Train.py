598d8bc6dd63f53e6be5a09b4423864630d50842
Brianwu-bsa <brianwu.bsa@gmail.com>
2025-08-26 22:17:40 -0400

    Added gradient accumulation, and smaller epsilon for better stability. Added back weight decay.,

--- Train.py
+++ Train.py
@@ -6,13 +6,17 @@ from Net.Stablemax import Stablemax
 from Net.Taylor_Softmax import Taylor_Softmax
 from Net.Alpha_Loss import Policy_Loss, Value_Loss, KLD
 from Net.Gumbel_Loss import Policy_Loss_Gumbel, Value_Loss_Gumbel, KLD_Gumbel
-def train(train_dataloader, test_dataloader, model, learning_rate, build_config, train_config):
+def train(train_dataloader, test_dataloader, model, learning_rate, build_config: dict, train_config:dict):
     # assume that save_folder path is Grok_Zero_Train/current_generation + 1
+    accum_steps = train_config.get("gradient_accumulation_steps")
+    if accum_steps is not None and accum_steps >= 2:
+        learning_rate /= accum_steps # this is to counter the gradients being scaled by accum_steps  thus we divide
     kwargs = {"learning_rate": learning_rate,
               "beta_1": train_config["beta_1"],
               "beta_2": train_config["beta_2"],
-              # "weight_decay": 1e-2,
+              "weight_decay": 1e-2,
               "gradient_accumulation_steps": train_config["gradient_accumulation_steps"],
+              "epsilon": 1e-15,
               }
 
     if train_config["optimizer"].lower() == "adam":
