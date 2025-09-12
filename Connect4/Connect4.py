f66d3f2c5192422ba6612c98f25609eb6d40b78a
Brianwu-bsa <brianwu.bsa@gmail.com>
2025-09-11 22:36:06 -0400

    kaggle update

--- Connect4/Connect4.py
+++ Connect4/Connect4.py
@@ -19,11 +19,11 @@ train_config = {
     "num_explore_actions_second": 7,  # Since player 1 is always at a disadvantage, we explore less and attempt to play better moves
 
     "use_gpu": True,  # Change this to False to use CPU for self play and inference
-    "use_tensorrt": True,  # Assuming use_gpu is True, uses TensorrtExecutionProvider
+    "use_tensorrt": False,  # Assuming use_gpu is True, uses TensorrtExecutionProvider
     # change this to False to use CUDAExecutionProvider
     "use_inference_server": True,  # if an extremely large model is used, because of memory constraints, set this to True
     "max_cache_depth": 4,  # maximum depth in the search of the neural networks outputs we should cache, use this if the inference speed is under 1000it/s
-    "num_workers": 8,  # Number of multiprocessing workers used to self play
+    "num_workers": 4,  # Number of multiprocessing workers used to self play
 
     # MCTS variables
     "MCTS_iteration_limit": 100,  # The number of iterations MCTS runs for. Should be 2 to 10x the number of starting legal moves
