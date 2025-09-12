6adaf2835940841bed27699dfcf9a3acfd423e96
Brianwu-bsa <brianwu.bsa@gmail.com>
2025-09-11 22:44:45 -0400

    kaggle update

--- Connect4/Connect4.py
+++ Connect4/Connect4.py
@@ -1,8 +1,8 @@
 import numpy as np
 from numba import njit
 
-build_config = {"num_resnet_layers": 2,  # This is the total amount of resnet layers in the model that are used
-                "num_filters": 128,
+build_config = {"num_resnet_layers": 4,  # This is the total amount of resnet layers in the model that are used
+                "num_filters": 256,
                 "use_stablemax": False,  # use stablemax, which will also use stablemax crossentropy
                 "use_grok_fast": False,  # from grokfast paper
                 "use_orthograd": False,  # from grokking at the edge of numerica stability
@@ -13,25 +13,25 @@ train_config = {
     # a generation is defined by a round of self play, padding the dataset, model training, converting to onnx
 
     # Self Play variables
-    "games_per_generation": 2000,  # amount of self play games until we re train the network
+    "games_per_generation": 10000,  # amount of self play games until we re train the network
     "max_actions": 42,  # Note that this should be less than max actions,
     "num_explore_actions_first": 8,  # A good rule of thumb is how long the opening should be for player -1
     "num_explore_actions_second": 7,  # Since player 1 is always at a disadvantage, we explore less and attempt to play better moves
 
-    "use_gpu": True,  # Change this to False to use CPU for self play and inference
+    "use_gpu": False,  # Change this to False to use CPU for self play and inference
     "use_tensorrt": False,  # Assuming use_gpu is True, uses TensorrtExecutionProvider
     # change this to False to use CUDAExecutionProvider
     "use_inference_server": True,  # if an extremely large model is used, because of memory constraints, set this to True
-    "max_cache_depth": 4,  # maximum depth in the search of the neural networks outputs we should cache, use this if the inference speed is under 1000it/s
-    "num_workers": 2,  # Number of multiprocessing workers used to self play
+    "max_cache_depth": 1,  # maximum depth in the search of the neural networks outputs we should cache, use this if the inference speed is under 1000it/s
+    "num_workers": 64,  # Number of multiprocessing workers used to self play
 
     # MCTS variables
-    "MCTS_iteration_limit": 100,  # The number of iterations MCTS runs for. Should be 2 to 10x the number of starting legal moves
+    "MCTS_iteration_limit": 300,  # The number of iterations MCTS runs for. Should be 2 to 10x the number of starting legal moves
     # True defaults to iteration_limit = 3 * len(starting legal actions)
     "MCTS_time_limit": None,  # Not recommended to use for training, True defaults to 30 seconds
     "use_njit": None,  # None will automatically infer what is supposed to be use for windows/linux
 
-    "use_gumbel": True,  # use gumbel according to https://openreview.net/pdf?id=bERaNdoegnO
+    "use_gumbel": False,  # use gumbel according to https://openreview.net/pdf?id=bERaNdoegnO
     # These params will only be used when use_gumbel is set to True
     "m": 7,  # Number of actions sampled in the first stage of sequential halving
     "c_visit": 50.0,
